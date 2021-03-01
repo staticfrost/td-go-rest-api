53c57f5a89f2608ab69600a585bac00f1fa89403
Scott Bailey <scott@scottbailey.net>
2021-03-01 21:54:20 +1000

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/staticfrost/td-go-rest-api/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers
 // to database connections
@@ -10,6 +15,15 @@ type App struct {
 // Run - this sets up the app
 func (app *App) Run() error {
 	fmt.Println("Setting up our App")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to setup server")
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/staticfrost/td-go-rest-api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
